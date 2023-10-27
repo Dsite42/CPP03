212004f0f782edb9ce0c675ed9d80a60f34a7d7e
cgodecke <cgodecke@student.42wolfsburg.de>
2023-10-27 10:31:25 +0200

    adapted print headers

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 14:42:04 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/10/09 12:26:28 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/10/27 10:28:11 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int main(void)
 
 
 	std::cout << "\033[33m" << std::endl << "Tests for ClapTrap functions" << "\033[0m" << std::endl;
+	std::cout << "Tim has " << tim.get_hit_points() << " hit points and " << tim.get_energy_points() << " energy points left" << std::endl;
 	tim.attack("a bear");
 	tim.takeDamage(2);
 	std::cout << "Tim has " << tim.get_hit_points() << " hit points and " << tim.get_energy_points() << " energy points left" << std::endl;
@@ -37,7 +38,7 @@ int main(void)
 	std::cout << "Bernd has " << bernd.get_hit_points() << " hit points and " << bernd.get_energy_points() << " energy points left" << std::endl;
 	
 
-	std::cout << "\033[33m" << std::endl << "Tests for ClapTrap copy assignment constructor" << "\033[0m" << std::endl;
+	std::cout << "\033[33m" << std::endl << "Tests for ClapTrap copy constructor and assignment operator" << "\033[0m" << std::endl;
 	std::cout << "CopyTim has " << copy_tim.get_hit_points() << " hit points and " << copy_tim.get_energy_points() << " energy points left" << std::endl;
 	copy_tim = tim;
 	std::cout << "CopyTim has " << copy_tim.get_hit_points() << " hit points and " << copy_tim.get_energy_points() << " energy points left" << std::endl;
